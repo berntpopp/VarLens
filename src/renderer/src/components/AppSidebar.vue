f205d85e01c83d6bc96eff578809387e121ea965
Bernt Popp <bernt.popp@gmail.com>
2026-01-26 19:16:43 +0100

    feat(05-01): add EmptyState and AppSidebar components
    - EmptyState displays centered welcome message with folder icon
    - AppSidebar wraps v-navigation-drawer with rail toggle
    - Sidebar collapses to rail with chevron button
    - Both components use Vue 3 Composition API with TypeScript

---
+++ src/renderer/src/components/AppSidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-navigation-drawer
+    permanent
+    :rail="rail"
+    :width="240"
+  >
+    <template v-slot:prepend>
+      <v-toolbar density="compact">
+        <v-toolbar-title v-show="!rail">Cases</v-toolbar-title>
+        <v-spacer />
+        <v-btn icon @click="toggleRail">
+          <v-icon>{{ rail ? 'mdi-chevron-right' : 'mdi-chevron-left' }}</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </template>
+
+    <slot />
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const rail = ref(false)
+
+const toggleRail = (): void => {
+  rail.value = !rail.value
+}
+
+defineExpose({ rail })
+</script>
---
+++ src/renderer/src/components/EmptyState.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="fill-height">
+    <v-row align="center" justify="center">
+      <v-col cols="12" class="text-center">
+        <v-icon size="120" color="grey-lighten-1">mdi-folder-open-outline</v-icon>
+        <h2 class="text-h5 mt-4 text-grey-darken-1">No Case Selected</h2>
+        <p class="text-body-1 mt-2 text-grey">
+          Import a variant file or select a case from the sidebar to view variants
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+// EmptyState: Welcome screen displayed when no case is selected
+</script>
